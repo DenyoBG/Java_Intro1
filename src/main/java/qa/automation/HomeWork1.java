2e24b2275ef18145786ca5120272b7bbab75d61a
DenyoBG <denisvelinov7@gmail.com>
2022-02-02 15:59:17 +0200

    Homework 1.0

--- src/main/java/qa/automation/HomeWork1.java
+++ src/main/java/qa/automation/HomeWork1.java
@@ -3,19 +3,29 @@ package qa.automation;
 import java.util.Scanner;
 
 public class HomeWork1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        System.out.println("Please enter your user name:");
+        System.out.println("Please enter your user name: ");
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String userName = scanner.next();
+        String userName = scanner.nextLine();
 
-        System.out.println("Please tell us your favourite hobbies:");
+        System.out.println("Please enter your favourite hobbies: ");
 
-        String hobby = scanner.next();
+        String hobby = scanner.nextLine();
 
-        System.out.println("User Name: " + userName + "\nFavourite hobbies: " + hobby);
+
+        System.out.println("Please enter your age: ");
+
+        int age = scanner.nextInt();
+
+        System.out.println("Please enter your user name: " + userName + "\nPlease enter your favourite hobbies: " + hobby + "" +
+                "\nPlease enter your age: " + age);
+
+
+
+
+
     }
 }
-
